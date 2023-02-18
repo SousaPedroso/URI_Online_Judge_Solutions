05777452304f9d2e0af7f1e974d57a1420c142dc
SousaPedroso <gsousa844@gmail.com>
2023-02-18 15:05:09 -0400

    C<=A para ser a menor solução

---
+++ ad-hoc/c++/1379.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    int a, b, c;
+    while(cin >> a >> b && a != 0){
+        c = (a-b)+a;
+        cout<< c<< endl;
+    }
+    return 0;
+}
